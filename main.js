0a2f3a5175d3fc577e67bbbefc36a9fed013c1e7
Szymon Łukaszuk <bezimienny.bezi@o2.pl>
2022-06-13 13:24:35 +0200

    Zrobione

---
+++ main.js
@@ -0,0 +1,10 @@
+function funkcja(n) {
+    if (n <200 & n > 100) {
+        console.log("Liczba znajduje się w przedziale");
+    }
+    else {
+        console.log("Liczba nie znajduje się w przedziale");
+    }
+}
+
+funkcja(122);
